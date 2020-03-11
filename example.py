7c52162ebd78d8941fc5376e53c01b632a87ecf0
Jerry Jia <42800895+JerryJiaGit@users.noreply.github.com>
2020-03-12 01:45:27 +0800

    Update v1.1
    v1.1 (03/12/2020) changes from v1.0
    - optimize func return
    - separate output file into func ExportScad()
    - remove ColorCode from func PixelCubeZDepth parameter
    - add func ModelMirror, func ModelTranslate, func ModelScale, func ModelColor

--- example.py
+++ example.py
@@ -14,9 +14,16 @@ v1.0 (03/11/2020) changes from initial release
 import picture2openscad
 
 debug_mode = False
-picture_filename = "example//Mount_Fuji.png"
+picture_filename = "example//HydrogenOrbitalsN6L0M0.png"
+scad_filename = "output.scad"
 
 pic2scad = picture2openscad.picture2openscad()
 im = pic2scad.ImportPicture(picture_filename, picture_gray_invert= True, picture_flip= "HORI",picture_norm_type="NORM_MINMAX" )
-pic2scad.PixelCubeZDepth(im, offset=[0,0,0], translate = [1,1,5], pixelcube = [1,1,1], zdepth = 0, exclude_threshold = 0, color_mode="GRAY", color_code=[.2,1,0.2,0.2])
+pic2scad.PixelCubeZDepth(im, offset=[0,0,0], translate = [1,1,0], pixelcube = [1,1,1], zdepth = 10, exclude_threshold = -1, color_mode="GRAY")
 pic2scad.ModelUnion()
+# pic2scad.ModelRotate(rotate_a=[0,0,1])
+# pic2scad.ModelScale(scale_v=[1,1,1])
+# pic2scad.ModelTranslate(translate_v=[1,1,1])
+# pic2scad.ModelMirror(mirror_v=[1,1,1])
+# pic2scad.ModelColor(color_c=[.2,1,0.2,0.2])
+pic2scad.ExportScad(scad_filename)
